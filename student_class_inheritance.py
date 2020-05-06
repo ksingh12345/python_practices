08f5a6b323cc1def7ad9edc2fc9206d4e6829c54
ksingh12345 <30820598+ksingh12345@users.noreply.github.com>
2020-05-07 01:50:26 +0530

    Update student_class_inheritance.py

--- student_class_inheritance.py
+++ student_class_inheritance.py
@@ -1,17 +1,20 @@
 class Student:
-    def __init__(self, name, school, mark):
+    def __init__(self, name, school):
         self.name = name
         self.school = school
-        self.mark = []
-
-
+        self.marks = []
+ 
+    @property
     def average(self):
-        return sum(self.mark)/len(self.mark)
-
+        return sum(self.marks) / len(self.marks)
+ 
 class WorkingStudent(Student):
     def __init__(self, name, school, salary):
         super().__init__(name, school)
         self.salary = salary
-
-
-rolf = WorkingStudent('Rolf', 'MIT', 15.50)
+ 
+ 
+rolf = WorkingStudent("Rolf", "MIT", 15.50)
+rolf.marks.append(57)
+ 
+print(rolf.average)
